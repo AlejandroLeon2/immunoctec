1d51aa588e7b8a30c259dec0f37fb68a001f9740
Pedro Alejandro <alejandroleonpedro7@gmail.com>
2025-10-24 18:54:42 -0500

    Primer avance

---
+++ src/types/tipos.ts
@@ -0,0 +1,16 @@
+export type whatsapp = {
+  numero: string;
+  mensaje?: string;
+};
+export interface Producto {
+  nombre: string;
+  titulo: string;
+  descripcion: string;
+  beneficios?:string[];
+  imagen: string;
+  color?:string;
+  textoBoton: string;
+  linkVideo?: string;
+  variant?:string;
+  badge?:string;
+};
