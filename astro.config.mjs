42d7db00ed509d53564d1223e5c2b51a18ce38fa
Pedro Alejandro <alejandroleonpedro7@gmail.com>
2025-10-28 00:14:37 -0500

    modificando el componente title

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,6 +7,7 @@ import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
+    site: 'http://soledadconsultoraimmunotec.com/',
   vite: {
     plugins: [tailwindcss()]
   },
